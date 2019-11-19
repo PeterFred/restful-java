0533e42ae1442e0d108aaedc304a9e8277fe5f00
PeterFred <32250775+PeterFred@users.noreply.github.com>
2019-11-19 15:31:34 +1300

    Update ScoreService.java

--- src/com/mcnz/restful/java/example/ScoreService.java
+++ src/com/mcnz/restful/java/example/ScoreService.java
@@ -5,6 +5,24 @@ import javax.ws.rs.*;
 public class ScoreService {
 	private static int wins, losses, ties;
 	
+	@GET	@Path("/score")	@Produces("application/json")
+	public String getScore() {
+		String pattern = "{ \"wins\":\"%s\", \"losses\":\"%s\", \"ties\": \"%s\"}";
+		return String.format(pattern, wins, losses, ties);
+	}
+	
+	//localhost:8080/restful-java/score?wins=2%losses=3%ties=15
+	@PUT	@Path("/score")	@Produces("application/json")
+	public String updateScore(@QueryParam("wins") int wins, 
+								@QueryParam("losses")int losses, 
+								@QueryParam("ties")int ties) {
+		ScoreService.wins = wins;
+		ScoreService.losses = losses;
+		ScoreService.ties = ties;
+		String pattern = "{ \"wins\":\"%s\", \"losses\":\"%s\", \"ties\": \"%s\"}";
+		return String.format(pattern, wins, losses, ties);
+	}
+	
 	@POST @Path("/score/wins")@Produces("text/plain")
 	public static int getWins() {
 		return wins;
