2411f9af7838c7b5a4b97d7a9ebae400ebd6c163
PeterFred <32250775+PeterFred@users.noreply.github.com>
2019-11-19 15:15:12 +1300

    ScoreService annotations added

--- src/com/mcnz/restful/java/example/ScoreService.java
+++ src/com/mcnz/restful/java/example/ScoreService.java
@@ -1,5 +1,38 @@
 package com.mcnz.restful.java.example;
 
+import javax.ws.rs.*;
+
 public class ScoreService {
+	private static int wins, losses, ties;
+	
+	@POST @Path("/score/wins")@Produces("text/plain")
+	public static int getWins() {
+		return wins;
+	}
+	
+	@POST @Path("/score/losses")@Produces("text/plain")
+	public static int getLosses() {
+		return losses;
+	}
+
+	@POST @Path("/score/ties")@Produces("text/plain")
+	public static int getTies() {
+		return ties;
+	}
+
+	@POST @Path("/score/wins")@Produces("text/plain")
+	public int increaseWins() {
+		return ++wins;
+	}
+	
+	@POST @Path("/score/losses")@Produces("text/plain")
+	public int increaseLosses() {
+		return ++losses;
+	}
+	
+	@POST @Path("/score/ties")@Produces("text/plain")
+	public int increaseTies() {
+		return ++ties;
+	}
 
 }
